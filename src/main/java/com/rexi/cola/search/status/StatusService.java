548f19d27560a5b878d83413b43f2ccad34981eb
Your Name <you@example.com>
2017-02-05 12:05:52 +0100

    Status

--- src/main/java/com/rexi/cola/search/status/StatusService.java
+++ src/main/java/com/rexi/cola/search/status/StatusService.java
@@ -1,7 +1,26 @@
 package com.rexi.cola.search.status;
 
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by moi on 05/02/2017.
  */
+@Service
 public class StatusService {
+
+    public final Client client;
+
+    @Autowired
+    public StatusService(Client client){
+        this.client = client;
+    }
+
+    public void getStatus(){
+
+        boolean isUp =  false ;
+    }
+
+
 }
