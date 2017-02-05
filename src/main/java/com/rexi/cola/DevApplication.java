568047b8e03937103f3bd9b75bb01fe7a98cd262
Your Name <you@example.com>
2017-02-05 11:07:16 +0100

    Begin new project

---
+++ src/main/java/com/rexi/cola/DevApplication.java
@@ -0,0 +1,12 @@
+package com.rexi.cola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+}
