5356a9ee3ea8ffa31caa19ef4e71c279c31483b5
Your Name <you@example.com>
2017-02-05 12:09:40 +0100

    POST

--- src/main/java/com/rexi/cola/search/status/Status.java
+++ src/main/java/com/rexi/cola/search/status/Status.java
@@ -4,4 +4,37 @@ package com.rexi.cola.search.status;
  * Created by moi on 05/02/2017.
  */
 public class Status {
+
+    public final boolean status;
+
+    public final String code;
+
+    public final String message;
+
+    public Status(boolean status, String code)
+    {
+        this(status, code, null);
+    }
+
+    public Status(boolean status, String code, String message)
+    {
+        this.status = status;
+        this.code   = code;
+        this.message = message;
+    }
+
+    public boolean getStatus()
+    {
+        return this.status;
+    }
+
+    public String getCode()
+    {
+        return this.code;
+    }
+
+    public String getMessage()
+    {
+        return this.message;
+    }
 }
